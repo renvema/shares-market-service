a4584499e8ad03166137e53932d70ba14e3f39b6
renvema <renvema@bigmir.net>
2024-04-24 18:05:37 +0200

    init commit

---
+++ src/main/java/com/renvema/sharesmarketservice/SharesMarketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.renvema.sharesmarketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharesMarketServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharesMarketServiceApplication.class, args);
+    }
+
+}
